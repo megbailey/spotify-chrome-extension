7f119790ec3b031540063ddac9d506a93dff3e93
Megan <megan@Megans-MacBook-Pro-2.local>
2018-06-02 00:44:15 -0700

    working on converting from node.js syntax to require.js

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 //TRYING AUTHORIZATION YIKES
 
-var express = require('express'); 
-var request = require('request');
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+var express = require(['express']); 
+var request = require(['request']);
+var cors = require(['cors']);
+var querystring = require(['querystring']);
+var cookieParser = require(['cookie-parser']);
 
 
 var client_id = '';
